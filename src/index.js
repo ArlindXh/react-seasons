6a2f0c8b11f56be49c7b6f1cfa7f2b8f2ebb9c50
ArlindX <Arlind>
2018-12-17 23:22:26 +0100

    second commit

--- src/index.js
+++ src/index.js
@@ -2,15 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-    render() {
+    constructor(props) {
+        super(props);
+
+        this.state = { lat: null };
+
         window.navigator.geolocation.getCurrentPosition(
             // successful callback
-            position => console.log(position),
+            position => {
+                //we called setstate
+                this.setState({ lat: position.coords.latitude })
+            },
             // failure
             err => console.log(err)
 
         )
-        return <div>Latitude:</div>
+    }
+
+    // Gotta define render!
+    render() {
+        return <div>Latitude {this.state.lat}</div>
     }
 }
 
